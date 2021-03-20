15072da5d9ed016292a428f8d004ff9df2955415
LizarKuo <mou76589@gmail.com>
2021-03-21 00:37:26 +0800

    v0.01
    上傳初版

---
+++ SendMail/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace SendMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmtpClient client = new SmtpClient();
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("寄件信箱", "寄件信箱密碼");
+
+            MailMessage message = new MailMessage();
+
+            message.From = new MailAddress("寄件地址", "寄件人名稱");
+            message.IsBodyHtml = true;
+            message.Subject = "C#信件主旨";
+            message.Body = "<h1>信件內容</h1>";
+            message.To.Add(new MailAddress("receiver mail"));
+            message.To.Add(new MailAddress("receiver mail2"));
+            message.CC.Add(new MailAddress("copy receiver mail"));
+            message.Bcc.Add(new MailAddress("bcc receiver mail"));
+            message.Attachments.Add(new Attachment([Attachment location(String Format)]));
+
+            client.Send(message);
+            message.Dispose();
+
+            Console.WriteLine("Send Mail!");
+        }
+    }
+}
